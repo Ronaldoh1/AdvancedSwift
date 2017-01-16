46a7b68c5ab3ccd75ed31b5bf10be7cd02e0d830
Ronald Hernandez <2ronald.hernandez@gmail.com>
2017-01-16 11:19:09 -0600

    added partial pattern matching

--- PatternMatching.playground/Contents.swift
+++ PatternMatching.playground/Contents.swift
@@ -2,4 +2,30 @@
 
 import UIKit
 
-var str = "Hello, playground"
+//let name = "twostraws"
+//
+//switch name {
+//case "billbo":
+//    print("Hello, Billbo")
+//case "twostraws":
+//    print("Hello, Ronald")
+//default:
+//    print("Authentication Fialed")
+//}
+
+
+// if we wanted to evuate a name and password  => you want to use a tuple
+
+let name = "twostraws"
+let password = "letmein123"
+
+let authentication = (name: "twostraws", password: "letmein123")
+
+switch authentication {
+case ("billbo", "bagins"):
+    print("Hello Billbo")
+case ("twostraws", "letmein123"):
+    print("Hi, Ronald How are you")
+default:
+    print("Authentication Failed, who are you?")
+}
