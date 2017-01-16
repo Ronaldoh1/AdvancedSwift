69dfe069325420716e46479d407222a2b9765e52
Ronald Hernandez <2ronald.hernandez@gmail.com>
2017-01-16 15:37:39 -0600

    added reversed methods

--- LazyProperties.playground/Contents.swift
+++ LazyProperties.playground/Contents.swift
@@ -5,6 +5,36 @@ import UIKit
 //lazy loading is one of the best system performance optimization. It's indemic in iOS. 
 
 
+//class Singer {
+//    let name: String
+//
+//    init(name: String) {
+//        self.name = name
+//    }
+//
+//    func reverseName() -> String {
+//        return String(name.uppercased().characters.reversed())
+//    }
+//}
+
+//let ronald = Singer(name: "Ronald")
+//ronald.reverseName()
+
+//each singer has a property called name. And amethod that does some work on that property. Everytime you want to print the name backward - you have to call the method again since the work done by reverse name is not stored anywhere. You could also have another property to store work. If you never called reversed. Now it cases your code to run slower if it's never used. 
+
+//lazy properties are only calculated once and stored. Never calculated if they aren't used. If your code access it repeatedly then only pay performance cost once. If they're never used then it's never run. 
+
+//lazy var view: UIView = {
+//    let view = UIView()
+//    return view
+//}()
+
+// self inside the closure will not cause any retain cycles. Swift is smart enough to know that self inside the closure is weak. 
+
+
+
+//say that you want to cut down on the code for each lazy property. you could create a separate method and make it private. like below. 
+
 class Singer {
     let name: String
 
@@ -12,21 +42,9 @@ class Singer {
         self.name = name
     }
 
-    func reverseName() -> String {
+    lazy var reversedName: String = self.reverseName1()
+
+    private func reverseName1() -> String {
         return String(name.uppercased().characters.reversed())
     }
 }
-
-let ronald = Singer(name: "Ronald")
-ronald.reverseName()
-
-//each singer has a property called name. And amethod that does some work on that property. Everytime you want to print the name backward - you have to call the method again since the work done by reverse name is not stored anywhere. You could also have another property to store work. If you never called reversed. Now it cases your code to run slower if it's never used. 
-
-//lazy properties are only calculated once and stored. Never calculated if they aren't used. If your code access it repeatedly then only pay performance cost once. If they're never used then it's never run. 
-
-lazy var view: UIView = {
-    let view = UIView()
-    return view
-}()
-
-// self inside the closure will not cause any retain cycles. Swift is smart enough to know that self inside the closure is weak. 
