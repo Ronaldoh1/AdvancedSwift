57d19dd8a695502642ee8f93bc46063d30463604
Ronald Hernandez <2ronald.hernandez@gmail.com>
2017-01-17 08:57:02 -0600

    add challenge 23

---
+++ Challenge23IntegerDisguisedAsString.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Write a function that accepts a string and returns true if it only contains numbers 0 through 9. 
+
+func hasOnlyDigits(_ input: String) -> Bool {
+    let characters = input.characters.map { String($0) }
+    for letter in characters {
+        if Int(letter) == nil {
+            return false
+        }
+    }
+    return true
+}
+
+// solution 1 
+func challenge23(input: String) -> Bool {
+    return UInt(input) != nil
+}
+
+// solution 2
+
+func challenge23b(input: String) -> Bool {
+    return input.rangeOfCharacter(from: CharacterSet(charactersIn: "0123456789").inverted) == nil
+}
