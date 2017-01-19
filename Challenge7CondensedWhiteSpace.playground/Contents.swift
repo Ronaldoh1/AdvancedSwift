42c29f85a28e8e7466c2df33e9ab330ddde7b6fa
Ronald Hernandez <2ronald.hernandez@gmail.com>
2017-01-19 06:58:48 -0600

    compelete condensed white space challenge

--- Challenge7CondensedWhiteSpace.playground/Contents.swift
+++ Challenge7CondensedWhiteSpace.playground/Contents.swift
@@ -2,4 +2,34 @@
 
 import UIKit
 
-var str = "Hello, playground"
+
+//Write a function that return a string with any consecutive spaces replaced with a single space. 
+// initial try
+func removeExtraSpacesAttemp1(input: String)  {
+
+    var characters = Array(input.characters)
+    var hasSeenSpace = false
+
+    for i in 0...characters.count {
+        if i > 0 && characters[i-1] == " " {
+            hasSeenSpace = true
+        }
+        if characters[i] == " " && hasSeenSpace {
+            characters.remove(at: i)
+        }
+
+    }
+
+    print(characters)
+  //  return characters
+}
+
+// Solution
+//We can use regular expressions to solve this. 
+
+func removeExtraSpaces(input: String) -> String {
+    let components = input.components(separatedBy: .whitespacesAndNewlines)
+    return components.filter{ !$0.isEmpty}.joined(separator: "")
+}
+
+removeExtraSpaces(input: "a   b   c")
