af23a1c5f29e94ccb30d399d8eaa25668dfefd02
Ronald Hernandez <2ronald.hernandez@gmail.com>
2017-01-28 09:01:14 -0600

    added left array rotation and apple stock problem

--- Swift-WrappingAnArray.playground/Contents.swift
+++ Swift-WrappingAnArray.playground/Contents.swift
@@ -6,42 +6,74 @@ import UIKit
 
 let tracks = ["a", "b", "c", "d", "e"]
 
-// algorithm should return a playlist if we select track "d"
+// algorithm should return a playlist if we select a track "d"
 
-var playlist = [String]() // this contains the selected track.
-let selectedTrack = "d" // this is our selected track
-var priorTracks = [String]() // this is the array prior to our selected track.
+["d", "e", "a", "b", "c"]
+
+let selectedSong = "d"
+
+var playlist = [String]()
+var priorTracks = [String]()
+
 
 for track in tracks {
 
-    if track == selectedTrack || playlist.count > 0 {
+    if track == selectedSong || playlist.count > 0 {
         playlist.append(track)
     } else {
         priorTracks.append(track)
     }
+
 }
-
-priorTracks
-
-// add the two arays together. 
 
 playlist + priorTracks
 
+// Solution without using a for loop 
 
-// Solution without using a for loop. 
 
-func indexOf(array:[String], selectedTrack: String) -> Int {
-    guard let index =  array.index(where: { return $0 == selectedTrack }) else {
-        return 0
-    }
-    return index
-}
 
-let index = indexOf(array: tracks, selectedTrack: selectedTrack)
 
-let prefix = tracks.prefix(upTo: index)
-let suffix = tracks.suffix(from: index)
 
- suffix + prefix
+
+
+
+
+//// algorithm should return a playlist if we select track "d"
+//
+//var playlist = [String]() // this contains the selected track.
+//let selectedTrack = "d" // this is our selected track
+//var priorTracks = [String]() // this is the array prior to our selected track.
+//
+//for track in tracks {
+//
+//    if track == selectedTrack || playlist.count > 0 {
+//        playlist.append(track)
+//    } else {
+//        priorTracks.append(track)
+//    }
+//}
+//
+//priorTracks
+//
+//// add the two arays together. 
+//
+//playlist + priorTracks
+//
+//
+//// Solution without using a for loop. 
+//
+//func indexOf(array:[String], selectedTrack: String) -> Int {
+//    guard let index =  array.index(where: { return $0 == selectedTrack }) else {
+//        return 0
+//    }
+//    return index
+//}
+//
+//let index = indexOf(array: tracks, selectedTrack: selectedTrack)
+//
+//let prefix = tracks.prefix(upTo: index)
+//let suffix = tracks.suffix(from: index)
+//
+// suffix + prefix
 
 
